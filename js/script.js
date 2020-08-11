d3fdbdb9a564a7079e6698488876c9db732b0ed8
YKalashnikov <uk2709@gmail.com>
2020-08-11 13:33:25 -0700

    changed 2

--- js/script.js
+++ js/script.js
@@ -2,13 +2,21 @@ const btn = document.getElementsByClassName("btn")[0];
 const modal = document.getElementsByClassName("modal")[0];
 const iframe = document.getElementsByTagName("iframe")[0]
 
-btn.onclick = function() {
-    modal.style.display = "block";
-  }
-  
-  document.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-      iframe.src = iframe.src
+
+const urlVideo = 'https://www.youtube.com/embed/pT7a87NND4I?rel=0&amp;autoplay=1'
+
+ function toggleModal() {
+       modal.style.display = "block";
+       iframe.src = urlVideo;
     }
-  }
+
+ function documentOnClick(event) {
+        if (event.target === modal) {
+             modal.style.display = "none";
+              iframe.src = ''
+        }
+    }
+
+
+btn.addEventListener("click", toggleModal);
+document.addEventListener("click", documentOnClick);
