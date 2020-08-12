d802e8a743c1eb28e018cf2f37645237abd0f0ad
YKalashnikov <uk2709@gmail.com>
2020-08-12 00:53:54 -0700

    adding closure

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
-const btn = document.getElementsByClassName("btn")[0];
-const modal = document.getElementsByClassName("modal")[0];
-const iframe = document.getElementsByTagName("iframe")[0]
+(function(){
 
-
-const urlVideo = 'https://www.youtube.com/embed/pT7a87NND4I?rel=0&amp;autoplay=1'
-
- function toggleModal() {
-       modal.style.display = "block";
-       iframe.src = urlVideo;
-    }
-
- function documentOnClick(event) {
-        if (event.target === modal) {
-             modal.style.display = "none";
-              iframe.src = ''
+    const btn = document.getElementsByClassName("btn")[0];
+    const modal = document.getElementsByClassName("modal")[0];
+    const iframe = document.getElementsByTagName("iframe")[0]
+    
+    
+    const urlVideo = 'https://www.youtube.com/embed/pT7a87NND4I?rel=0&amp;autoplay=1'
+    
+     function toggleModal() {
+           modal.style.display = "block";
+           iframe.src = urlVideo;
         }
-    }
+    
+     function documentOnClick(event) {
+            if (event.target === modal) {
+                 modal.style.display = "none";
+                  iframe.src = ''
+            }
+        }
+    
+    
+    btn.addEventListener("click", toggleModal);
+    document.addEventListener("click", documentOnClick);
 
 
-btn.addEventListener("click", toggleModal);
-document.addEventListener("click", documentOnClick);
+
+})()
